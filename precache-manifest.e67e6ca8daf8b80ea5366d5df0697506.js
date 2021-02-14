aa27e32d89b6793754745e9807eb6e14c0a60af6
Gabriel Bernardino <gabriel.bernardino1@gmail.com>
2021-02-14 20:55:58 +0100

    Updates

---
+++ precache-manifest.e67e6ca8daf8b80ea5366d5df0697506.js
@@ -0,0 +1,25 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/RVparcellation/6db3c7cd7f93fe8b5cb09b2d2a4af18f.wasm"
+  },
+  {
+    "revision": "c0f2fc3afa3bc8a9f4f0a2bef3d58f9d",
+    "url": "/RVparcellation/index.html"
+  },
+  {
+    "revision": "1f587c6d42bbcba59831",
+    "url": "/RVparcellation/static/css/main.5e87d105.chunk.css"
+  },
+  {
+    "revision": "63c2520912b9beeaead4",
+    "url": "/RVparcellation/static/js/2.f9e9d4a1.chunk.js"
+  },
+  {
+    "revision": "1f587c6d42bbcba59831",
+    "url": "/RVparcellation/static/js/main.469a6667.chunk.js"
+  },
+  {
+    "revision": "b03ba58bc87f0d279d9c",
+    "url": "/RVparcellation/static/js/runtime~main.bfa7f08c.js"
+  }
+]);
